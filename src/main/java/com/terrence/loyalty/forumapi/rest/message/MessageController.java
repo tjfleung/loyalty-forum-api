f63061eb6dd1731b5034e20dc9de02444dffcd7a
Terrence Leung <tjfleung@live.ca>
2019-12-28 21:30:11 -0500

    Step 3: CrossOrigin and log message string

--- src/main/java/com/terrence/loyalty/forumapi/rest/message/MessageController.java
+++ src/main/java/com/terrence/loyalty/forumapi/rest/message/MessageController.java
@@ -1,13 +1,17 @@
 package com.terrence.loyalty.forumapi.rest.message;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/message")
+@Slf4j
 public class MessageController {
 
+    @CrossOrigin
     @GetMapping
     public String getMessage(@RequestParam String message) {
+        log.info(message);
         return message;
     }
 }
