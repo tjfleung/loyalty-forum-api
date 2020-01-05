9c2cce4de57eb9012fdd68e02ddb0d9ea8d6b001
Terrence Leung <tjfleung@live.ca>
2020-01-05 16:23:18 -0500

    Step 6: allow replies to existing messages

--- src/main/java/com/terrence/loyalty/forumapi/domain/message/MessageDto.java
+++ src/main/java/com/terrence/loyalty/forumapi/domain/message/MessageDto.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,6 +15,8 @@ public class MessageDto {
     private String username;
     private String comment;
     private LocalDateTime postedDate;
+    private long parentId;
+    private List<MessageDto> childMessages = new ArrayList<>();
 
     public MessageDto() {}
 
@@ -20,6 +24,29 @@ public class MessageDto {
         this.username = username;
         this.comment = comment;
         this.postedDate = postedDate;
+    }
+
+    public MessageDto(String username, LocalDateTime postedDate, String comment, long parentId) {
+        this.username = username;
+        this.comment = comment;
+        this.postedDate = postedDate;
+        this.parentId = parentId;
+    }
+
+    public MessageDto(String username, LocalDateTime postedDate, String comment, long parentId, List<MessageDto> childMessages) {
+        this.username = username;
+        this.comment = comment;
+        this.postedDate = postedDate;
+        this.parentId = parentId;
+        this.childMessages = childMessages;
+    }
+
+    public void addChildMessage(MessageDto childMessage) {
+        this.childMessages.add(childMessage);
+    }
+
+    public void addChildMessages(List<MessageDto> childMessages) {
+        this.childMessages.addAll(childMessages);
     }
 
 }
--- src/main/java/com/terrence/loyalty/forumapi/domain/message/MessageService.java
+++ src/main/java/com/terrence/loyalty/forumapi/domain/message/MessageService.java
@@ -4,11 +4,11 @@ import com.terrence.loyalty.forumapi.exception.ForumException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,10 +28,19 @@ public class MessageService {
     }
 
     public List<MessageDto> getAllMessages() {
-        return messageRepository.findAll(Sort.by(Sort.Direction.DESC, "postedDate"))
-                .stream()
-                .map(message -> modelMapper.map(message, MessageDto.class))
-                .collect(Collectors.toList());
+        List<MessageDto> parentMessageDtos = new ArrayList<>();
+        parentMessageDtos.addAll(
+                messageRepository.findAllByParentIdEqualsOrderByPostedDateDesc(0)
+                        .stream()
+                        .map(message -> modelMapper.map(message, MessageDto.class))
+                        .collect(Collectors.toList())
+        );
+
+        for (MessageDto parent : parentMessageDtos) {
+            parent.addChildMessages(findChildMessages(parent.getId()));
+        }
+
+        return parentMessageDtos;
     }
 
     public MessageDto save(MessageDto messageDto) throws ForumException {
@@ -39,7 +48,7 @@ public class MessageService {
             throw new ForumException(HttpStatus.BAD_REQUEST, "No username provided");
         }
         if (messageDto.getComment() == null || messageDto.getComment().isEmpty()) {
-            throw new ForumException(HttpStatus.BAD_REQUEST, "No message to post");
+            throw new ForumException(HttpStatus.BAD_REQUEST, "No comment to post");
         }
 
         Message savedMessage = messageRepository.save(new Message(messageDto.getUsername(), LocalDateTime.now(), messageDto.getComment()));
@@ -52,5 +61,58 @@ public class MessageService {
                 .stream()
                 .map(message -> modelMapper.map(message, MessageDto.class))
                 .collect(Collectors.toList());
+    }
+
+    public MessageDto saveReply(MessageDto messageDto, long parentId) {
+        if (parentId <= 0) {
+            throw new ForumException(HttpStatus.BAD_REQUEST, "Invalid comment to reply");
+        }
+        if (messageDto.getUsername() == null || messageDto.getUsername().isEmpty()) {
+            throw new ForumException(HttpStatus.BAD_REQUEST, "No username provided");
+        }
+        if (messageDto.getComment() == null || messageDto.getComment().isEmpty()) {
+            throw new ForumException(HttpStatus.BAD_REQUEST, "No comment to post");
+        }
+
+        Message reply = messageRepository.save(new Message(messageDto.getUsername(), LocalDateTime.now(), messageDto.getComment(), parentId));
+        log.info("Saved new reply: {}, from {}, replying to Message {}", reply.getComment(), reply.getUsername(), reply.getParentId());
+        return modelMapper.map(reply, MessageDto.class);
+    }
+
+    private List<MessageDto> findChildMessages(long parentId) {
+        List<Message> childMessages = messageRepository.findAllByParentIdEqualsOrderByPostedDateDesc(parentId);
+        if (childMessages == null) {
+            return null;
+        }
+
+        List<MessageDto> childDtos = childMessages
+                .stream()
+                .map(message -> modelMapper.map(message, MessageDto.class))
+                .collect(Collectors.toList());
+
+        for (MessageDto parent : childDtos) {
+            parent.addChildMessages(findChildMessages(parent.getId()));
+        }
+
+        return childDtos;
+    }
+
+
+    private List<MessageDto> findChildMessages(String username, long parentId) {
+        List<Message> childMessages = messageRepository.findAllByUsernameAndParentIdOrderByPostedDateDesc(username, parentId);
+        if (childMessages == null) {
+            return null;
+        }
+
+        List<MessageDto> childDtos = childMessages
+                .stream()
+                .map(message -> modelMapper.map(message, MessageDto.class))
+                .collect(Collectors.toList());
+
+        for (MessageDto parent : childDtos) {
+            parent.addChildMessages(findChildMessages(username, parent.getId()));
+        }
+
+        return childDtos;
     }
 }
